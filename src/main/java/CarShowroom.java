8ab1e20f69d5e766ad12e66d01f7c4afae718039
Alexey Khmelev <a.khmelev91@gmail.com>
2021-09-08 07:54:04 +1000

    Car Showroom with lock

--- src/main/java/CarShowroom.java
+++ src/main/java/CarShowroom.java
@@ -1,36 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class CarShowroom {
-    public static final int THREAD_TIMEOUT = 2500;
+    private static final int THREAD_TIMEOUT = 2500;
+    private static final int NEW_CLIENT_TIMEOUT = 1000;
 
-    List<Car> cars = new ArrayList<>(10);
+    private List<Car> cars = new ArrayList<>(10);
 
-    public synchronized void receiveCar() {
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
+
+    public void receiveCar() {
         while (!Thread.currentThread().isInterrupted() && cars.size() < 10) {
             try {
+                lock.lock();
                 Thread.sleep(THREAD_TIMEOUT);
                 cars.add(new Car());
                 System.out.printf("Производитель выпустил новое авто марки %s\n", Thread.currentThread().getName());
-                notify();
+                condition.signal();
             } catch (InterruptedException ex) {
                 System.out.println(ex.getMessage());
+            } finally {
+                lock.unlock();
             }
         }
     }
 
-    public synchronized void sellCar() {
+    public void sellCar() {
         try {
+            lock.lock();
             System.out.printf("%s зашел в магазин\n", Thread.currentThread().getName());
             while (cars.size() == 0) {
                 System.out.println("Машин нет");
-                wait();
+                condition.await();
             }
             Thread.sleep(THREAD_TIMEOUT);
             System.out.printf("%s покупатель уехал новеньком авто\n", Thread.currentThread().getName());
             cars.remove(0);
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
+        } finally {
+            lock.unlock();
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.UnaryOperator;
+
 
 public class Main {
     public static void main(String[] args) {
         final CarShowroom showroom = new CarShowroom();
 
-        new Thread(null, showroom::sellCar, "Покупатель 1").start();
-        new Thread(null, showroom::sellCar, "Покупатель 2").start();
-        new Thread(null, showroom::sellCar, "Покупатель 4").start();
-        new Thread(null, showroom::sellCar, "Покупатель 5").start();
-        new Thread(null, showroom::sellCar, "Покупатель 6").start();
-        new Thread(null, showroom::sellCar, "Покупатель 7").start();
-        new Thread(null, showroom::sellCar, "Покупатель 8").start();
-        new Thread(null, showroom::sellCar, "Покупатель 9").start();
-        new Thread(null, showroom::sellCar, "Покупатель 10").start();
+        for (int i = 0; i < 10; i++) {
+            new Thread(null, showroom::sellCar, "Покупатель " + i).start();
+        }
 
         new Thread(null, showroom::receiveCar, "Mazda").start();
     }
